ae249cadf634d8f725979bc13dc48144f5e76417
Rishabh Shukla <43636003+RishabhShukla2000@users.noreply.github.com>
2022-09-23 12:58:08 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program 
+{
+    void print(int i, int j)
+    {
+        Console.WriteLine("Sum of 2 numbers : {0}", i + j);
+    }
+    void print(string a, string b)
+    {
+        Console.WriteLine("String Concatenation : " + a + b);
+    }
+    static void Main(string[] args)
+    {
+        Program prog = new Program();
+        prog.print(7, 5);
+        prog.print("Hello", "World!!");
+
+        Q3 tech = new Q3();
+        String descr = tech.Describe();
+
+        Console.WriteLine(descr);
+        Console.ReadKey();
+    }
+}
+
+abstract class Company
+{
+    public virtual string Describe()
+    {
+        return "Work of Company";
+    }
+}
+
+class Q3 : Company
+{
+    public override string Describe()
+    {
+        return "Work of Q3 is to build quality software";
+    }
+}
